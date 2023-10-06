d057c1749753683c0550703cbdb81904f4931383
GiorgosG <giorgos.gioumertakis@bel.gr>
2023-10-06 13:51:18 +0300

    first commit

---
+++ blog-dotnet-api/BlogApplicationApi/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("This is a get request");
+        }
+    }
+}
---
+++ blog-dotnet-api/BlogApplicationApi/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Types.Models;
+
+namespace Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<ICollection<User>> GetAll();
+
+        Task<User> GetUserById(int userId);
+
+        Task<User> AddUser(User toCreate);
+
+        Task<User> UpdateUser(int userId, string name, string email);
+
+        Task DeleteUser(int userId);
+    }
+}
